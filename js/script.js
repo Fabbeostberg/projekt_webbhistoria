599e5a7c607da5e862e104fa11a0edea08b544e7
Fabbeostberg <54536064+Fabbeostberg@users.noreply.github.com>
2019-12-09 15:35:05 +0100

    12 dec
    Har läst om java och lagt till bilder

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
-console.log("hejsan");
+function mode() {
+  console.log("hejsan");
+  var element = document.body;
+  element.classList.toggle("dark-mode");
+
+  var navbar = document.getElementById("navbar");
+  var content = document.getElementsByClassName("container-text")[0];
+
+  content.classList.toggle("dark-mode");
+}
