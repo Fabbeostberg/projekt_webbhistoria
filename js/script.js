ee7f75c2f9a2f0a7a9e422ca46ae357d2614f5fe
Fabbeostberg <54536064+Fabbeostberg@users.noreply.github.com>
2020-01-13 15:46:33 +0100

    Sista lektionen
    Gjort klart alla sidor

--- js/script.js
+++ js/script.js
@@ -1,10 +1,6 @@
 function mode() {
   console.log("hejsan");
-  var element = document.body;
-  element.classList.toggle("dark-mode");
 
-  var navbar = document.getElementById("navbar");
-  var content = document.getElementsByClassName("container-text")[0];
-
-  content.classList.toggle("dark-mode");
+  document.getElementById("Yeah").innerHTML +=
+    " Good job you just took order from a button";
 }
